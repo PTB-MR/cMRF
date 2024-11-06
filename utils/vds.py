b27844c05999ca948ebb07f74f7fdeb1ad1c4de4
Patrick Schuenke <patrick.schuenke@gmail.com>
2024-11-06 10:00:34 +0100

    refactor and rename vds functions

--- utils/vds.py
+++ utils/vds.py
@@ -1,281 +1,264 @@
-"""Function to generate a variable density spiral trajectory.
+"""Functions to generate a variable density spiral trajectory.
 
-Program translated from the matlab Program of Brian Hargreaves:
+The code is originally based on MATLAB code of Brian Hargreaves:
 http://mrsrl.stanford.edu/~brian/vdspiral/
-
-Following modifications have been made:
-- Adaptation was also made from the original program to change the default unit of the matlab program
-and were corrected in the description of the program underneath
--> In this new version the smax and gmax values are given in Hz/m/s and Hz/m/s and already consider the gamma factor
-- Oversampling is now a parameter of the function
-- Possibility to turn off/on plots and prints with a boolean
-
-#Description given by Brian Hargreaves and where units were adapted to the modifications done
-%	function [k,g,s,time,r,theta] = vds(smax,gmax,T,N,Fcoeff,rmax)
-%
-%	VARIABLE DENSITY SPIRAL GENERATION:
-%	----------------------------------
-%
-%	Function generates variable density spiral which traces
-%	out the trajectory
-%
-%			k(t) = r(t) exp(i*q(t)), 		[1]
-%
-%	Where q is the same as theta...
-%		r and q are chosen to satisfy:
-%
-%		1) Maximum gradient amplitudes and slew rates.
-%		2) Maximum gradient due to FOV, where FOV can
-%		   vary with k-space radius r/rmax, as
-%
-%			FOV(r) = Sum    Fcoeff(k)*(r/rmax)^(k-1)   [2]
-%
-%
-%	INPUTS:
-%	-------
-%	smax = maximum slew rate in Hz/m/s
-%	gmax = maximum gradient in Hz/m (limited by Gmax or FOV)
-%	T = sampling period (s) for gradient AND acquisition.
-%	N = number of interleaves.
-%	Fcoeff = FOV coefficients with respect to r - see above.
-%	rmax= value of k-space radius at which to stop (m^-1).
-%		rmax = 1/(2*resolution)
-%
-%
-%	OUTPUTS:
-%	--------
-%	k = k-space trajectory (kx+iky) in m-1.
-%	g = gradient waveform (Gx+iGy) in Hz/m.
-%	s = derivative of g (Sx+iSy) in Hz/m/s.
-%	time = time points corresponding to above (s).
-%	r = k-space radius vs time (used to design spiral)
-%	theta = atan2(ky,kx) = k-space angle vs time.
-%
-%
-%	METHODS:
-%	--------
-%	Let r1 and r2 be the first derivatives of r in [1].
-%	Let q1 and q2 be the first derivatives of theta in [1].
-%	Also, r0 = r, and q0 = theta - sometimes both are used.
-%	F = F(r) defined by Fcoeff.
-%
-%	Differentiating [1], we can get G = a(r0,r1,q0,q1,F)
-%	and differentiating again, we get S = b(r0,r1,r2,q0,q1,q2,F)
-%
-%	(functions a() and b() are reasonably easy to obtain.)
-%
-%	FOV limits put a constraint between r and q:
-%
-%		dr/dq = N/(2*pi*F)				[3]
-%
-%	We can use [3] and the chain rule to give
-%
-%		q1 = 2*pi*F/N * r1				[4]
-%
-%	and
-%
-%		q2 = 2*pi/N*dF/dr*r1^2 + 2*pi*F/N*r2		[5]
-%
-%
-%
-%	Now using [4] and [5], we can substitute for q1 and q2
-%	in functions a() and b(), giving
-%
-%		G = c(r0,r1,F)
-%	and 	S = d(r0,r1,r2,F,dF/dr)
-%
-%
-%	Using the fact that the spiral should be either limited
-%	by amplitude (Gradient or FOV limit) or slew rate, we can
-%	solve
-%		|c(r0,r1,F)| = |Gmax|  				[6]
-%
-%	analytically for r1, or
-%
-%	  	|d(r0,r1,r2,F,dF/dr)| = |Smax|	 		[7]
-%
-%	analytically for r2.
-%
-%	[7] is a quadratic equation in r2.  The smaller of the
-%	roots is taken, and the np.real part of the root is used to
-%	avoid possible numeric errors - the roots should be np.real
-%	always.
-%
-%	The choice of whether or not to use [6] or [7], and the
-%	solving for r2 or r1 is done by findq2r2 - in this .m file.
-%
-%	Once the second derivative of theta(q) or r is obtained,
-%	it can be integrated to give q1 and r1, and then integrated
-%	again to give q and r.  The gradient waveforms follow from
-%	q and r.
-%
-%	Brian Hargreaves -- Sept 2000.
-%
-%	See Brian's journal, Vol 6, P.24.
-%
-% ===========================================================
 """
 
 import numpy as np
+import pypulseq as pp
 
 
-def qdf(a: float, b: float, c: float) -> tuple[float, float]:
-    """Return the roots of a 2nd degree polynom ax**2+bx+c.
+def quadratic_formula_solver(a: float, b: float, c: float) -> tuple[float, float]:
+    """Return the roots of a quadratic equation in the form ax^2 + bx + c = 0.
 
     Parameters
     ----------
     a : float
+        Coefficient of the x^2 term.
     b : float
-    c: float
+        Coefficient of the x term.
+    c : float
+        Constant term of the equation.
 
     Returns
     -------
-    tuple(root1, root2): tuple(float,float)
+    tuple(float, float)
+        The two roots (solutions) of the quadratic equation.
     """
-    d = b**2 - 4 * a * c
-    roots = ((-b + np.sqrt(d)) / (2 * a), (-b - np.sqrt(d)) / (2 * a))
+    discriminant = b**2 - 4 * a * c
+    root1 = (-b + np.sqrt(discriminant)) / (2 * a)
+    root2 = (-b - np.sqrt(discriminant)) / (2 * a)
 
-    return roots
+    return root1, root2
 
 
-def findq2r2(
-    smax: float,
-    gmax: float,
-    r: float,
-    r1: float,
-    T: float,
-    Ts: float,
-    N: int,
-    Fcoeff: list,
-    rmax: float,
+def calculate_angular_and_radial_acceleration(
+    max_slew: float,
+    max_grad: float,
+    radius: float,
+    radius_derivative: float,
+    sampling_period: float,
+    sampling_period_os: float,
+    n_interleaves: int,
+    fov_coefficients: list,
+    max_kspace_radius: float,
 ) -> tuple[float, float]:
-    """Help function for vds.
-
-    The function calculates the second derivative of the angle theta (q) and the second
-    derivative of the radius r in the spiral trajectory to be integrated to
-    have the angle and radius increment.
+    """Calculate second derivatives of angle (theta) and radius (r) for a VDS trajectory.
 
     Parameters
     ----------
-    smax : float - maximal slew rate of the system in Hz/m/s
-    gmax : float - maximal gradient amplitude in Hz/m
-    r : float - radius of the spiral being constructed in m
-    r1 : float - derivative of the radius of the spiral being constructed in m
-    T : float - sampling period (s) for gradient AND acquisition.
-    Ts : float - sampling period (s) for gradient AND acquisition divided by an oversampling period
-    N : int - number of interleaves
-    Fcoeff : list - numbers between which the FOV varies
-    rmax : float - maximal radius in k-sapce  in m^(-1)
+    max_slew : float
+        Maximum slew rate of the system in Hz/m/s.
+    max_grad : float
+        Maximum gradient amplitude in Hz/m.
+    radius : float
+        Current radius of the spiral being constructed in meters.
+    radius_derivative : float
+        Derivative of the radius (rate of change) of the spiral in meters.
+    sampling_period : float
+        Sampling period (s) for gradient and acquisition.
+    sampling_period_os : float
+        Sampling period (s) for gradient and acquisition, divided by an oversampling factor.
+    n_interleaves : int
+        Number of spiral arms (interleaves).
+    fov_coefficients : list
+        List of coefficients defining the Field of View (FOV) profile.
+    max_kspace_radius : float
+        Maximum radius in k-space in m^(-1).
 
     Returns
     -------
-    tuple(q2, r2): tuple(float,float) - rad/s^(-2), m/s^(-2)
+    tuple[float, float]
+        Angular acceleration (q2) in rad/s^(-2) and radial acceleration (r2) in m/s^(-2).
     """
-    F = 0  # FOV function value for this r.
-    dFdr = 0  # dFOV/dr for this value of r.
+    # Initialize Field of View (fov) value and its derivative
+    fov = 0
+    fov_derivative = 0
 
-    for rind in range(len(Fcoeff)):
-        F += Fcoeff[rind] * (r / rmax) ** rind
-        if rind > 0:
-            dFdr += rind * Fcoeff[rind] * (r / rmax) ** (rind - 1) / rmax
+    # Calculate fov and its derivative based on radius and fov_coefficients
+    for index, coefficient in enumerate(fov_coefficients):
+        fov += coefficient * (radius / max_kspace_radius) ** index
+        if index > 0:
+            fov_derivative += index * coefficient * (radius / max_kspace_radius) ** (index - 1) / max_kspace_radius
 
-    GmaxFOV = 1 / F / Ts
-    Gmax = min(GmaxFOV, gmax)
+    # Determine adjusted maximum gradient amplitude based on fov
+    max_grad_for_fov = 1 / fov / sampling_period_os
+    adjusted_max_gradient = min(max_grad_for_fov, max_grad)
 
-    maxr1 = np.sqrt(Gmax**2 / (1 + (2 * np.pi * F * r / N) ** 2))
-    if r1 > maxr1:
-        # Grad amplitude limited.  Here we just run r upward as much as we can without
-        # going over the max gradient.
-        r2 = (maxr1 - r1) / T
+    # Limit radius derivative based on adjusted maximum gradient
+    max_radius_derivative = np.sqrt(adjusted_max_gradient**2 / (1 + (2 * np.pi * fov * radius / n_interleaves) ** 2))
+
+    # Determine radial acceleration based on gradient limit
+    if radius_derivative > max_radius_derivative:
+        # Adjust radial acceleration to stay within max gradient amplitude
+        radial_acceleration = (max_radius_derivative - radius_derivative) / sampling_period
     else:
-        twopiFoN = 2 * np.pi * F / N
-        twopiFoN2 = twopiFoN**2
-        # A, B, C are coefficients of the equation which equates the slew rate
-        # calculated from r, r1, r2 with the maximum gradient slew rate.
-        # A * r2 * r2 + B * r2 + C = 0
-        # A, B, C are in terms of F, dF / dr, r, r1, N and smax.
-        A = r * r * twopiFoN2 + 1
-        B = 2 * twopiFoN2 * r * r1 * r1 + 2 * twopiFoN2 / F * dFdr * r * r * r1 * r1
-        C = (
-            twopiFoN2**2 * r * r * r1**4
-            + 4 * twopiFoN2 * r1**4
-            + (2 * np.pi / N * dFdr) ** 2 * r * r * r1**4
-            + 4 * twopiFoN2 / F * dFdr * r * r1**4
-            - smax**2
+        # Calculate frequency values for angular acceleration calculation
+        angular_freq_over_interleaves = 2 * np.pi * fov / n_interleaves
+        angular_freq_squared = angular_freq_over_interleaves**2
+
+        # Calculate coefficients for radial acceleration equation under maximum slew rate constraint
+        a = radius**2 * angular_freq_squared + 1
+        b = (
+            2 * angular_freq_squared * radius * radius_derivative**2
+            + 2 * angular_freq_squared / fov * fov_derivative * radius**2 * radius_derivative**2
         )
-        rts = qdf(A, B, C)
-        r2 = np.real(rts[0])
-        slew = r2 - twopiFoN2 * r * r1**2 + 1j * twopiFoN * (2 * r1**2 + r * r2 + dFdr / F * r * r1**2)
-        sr = np.abs(slew) / smax
+        c = (
+            angular_freq_squared**2 * radius**2 * radius_derivative**4
+            + 4 * angular_freq_squared * radius_derivative**4
+            + (2 * np.pi / n_interleaves * fov_derivative) ** 2 * radius**2 * radius_derivative**4
+            + 4 * angular_freq_squared / fov * fov_derivative * radius * radius_derivative**4
+            - max_slew**2
+        )
 
-        if np.abs(slew) / smax > 1.01:
-            print('Slew violation, slew = ', round(abs(slew)), ' smax= ', round(smax), ' sr=', sr, ' r=', r, ' r1=', r1)
+        # Solve for radial acceleration (r2)
+        roots = quadratic_formula_solver(a, b, c)
+        radial_acceleration = np.real(roots[0])
 
-    q2 = 2 * np.pi / N * dFdr * r1**2 + 2 * np.pi * F / N * r2
+        # Calculate actual slew rate and check for violations
+        _tmp1 = 1j * angular_freq_over_interleaves
+        _tmp2 = (
+            2 * radius_derivative**2
+            + radius * radial_acceleration
+            + fov_derivative / fov * radius * radius_derivative**2
+        )
 
-    return q2, r2
+        slew_rate_vector = radial_acceleration - angular_freq_squared * radius * radius_derivative**2 + _tmp1 * _tmp2
+        slew_rate_ratio = np.abs(slew_rate_vector) / max_slew
+
+        # Print warning if slew rate violation detected
+        if slew_rate_ratio > 1.0 + 1e-6:
+            print(
+                f'Slew rate violation detected for radius = {radius}.\n'
+                f'Current slew rate = {round(np.abs(slew_rate_vector))} '
+                f'Maximum slew rate = {round(max_slew)} (ratio = {round(slew_rate_ratio)})\n'
+            )
+
+    # Calculate angular acceleration (q2)
+    angular_acceleration = (
+        2 * np.pi / n_interleaves * fov_derivative * radius_derivative**2
+        + 2 * np.pi * fov / n_interleaves * radial_acceleration
+    )
+
+    return angular_acceleration, radial_acceleration
 
 
-def vds(
-    smax: float,
-    gmax: float,
-    T: float,
-    N: int,
-    Fcoeff: list,
-    rmax: float,
-    oversampling: int,
+def variable_density_spiral_trajectory(
+    system: pp.Opts,
+    sampling_period: float,
+    n_interleaves: int,
+    fov_coefficients: list,
+    max_kspace_radius: float,
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    """Calculate the variable density spiral trajectory."""
-    # calculate time step with oversampling
-    delta_t_os = T / oversampling
+    """Generate a variable density spiral (VDS) trajectory.
 
-    # Initialize variables
-    q0 = q1 = r0 = r1 = 0
+    Parameters
+    ----------
+    system : pp.Opts
+        PyPulseq system object containing gradient and slew rate limits.
+    sampling_period : float
+        Base sampling period for gradient and acquisition.
+    n_interleaves : int
+        Number of spiral arms (interleaves) in the trajectory.
+    fov_coefficients : list
+        Coefficients defining the Field of View (FOV) profile.
+    max_kspace_radius : float
+        Maximum k-space radius in inverse meters.
+
+    Returns
+    -------
+    tuple(np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray)
+        - k-space trajectory (k)
+        - Gradient waveform (g)
+        - Slew rate (s)
+        - Time points for the trajectory (time)
+        - Radius values (r)
+        - Angular positions (theta)
+    """
+    # Extract system limits from PyPulseq system object
+    max_slew = system.max_slew * 0.9
+    max_grad = system.max_grad * 0.9
+
+    # Define oversampling factor for finer time resolution during calculations
+    oversampling_factor = 12
+    sampling_period_os = sampling_period / oversampling_factor
+
+    # Initialize angular and radial positions and derivatives
+    angular_position = 0
+    angular_velocity = 0
+    radial_position = 0
+    radial_velocity = 0
 
     # Initialize lists for storing the trajectory
-    theta = [q0]
-    r = [r0]
+    angular_positions = [angular_position]
+    radial_positions = [radial_position]
 
-    while r0 < rmax:
-        q2, r2 = findq2r2(smax, gmax, r0, r1, delta_t_os, T, N, Fcoeff, rmax)
+    while radial_position < max_kspace_radius:
+        q2, r2 = calculate_angular_and_radial_acceleration(
+            max_slew,
+            max_grad,
+            radial_position,
+            radial_velocity,
+            sampling_period_os,
+            sampling_period,
+            n_interleaves,
+            fov_coefficients,
+            max_kspace_radius,
+        )
 
         # Integrate for r, r', theta and theta'
-        q1 = q1 + q2 * delta_t_os
-        q0 = q0 + q1 * delta_t_os
+        angular_velocity += q2 * sampling_period_os
+        angular_position += angular_velocity * sampling_period_os
 
-        r1 += r2 * delta_t_os
-        r0 += r1 * delta_t_os
+        radial_velocity += r2 * sampling_period_os
+        radial_position += radial_velocity * sampling_period_os
 
-        # Store
-        theta.append(q0)
-        r.append(r0)
+        # Append current positions to the lists
+        angular_positions.append(angular_position)
+        radial_positions.append(radial_position)
 
-    count = len(r)
-    theta = np.array(theta)[:, np.newaxis]
-    r = np.array(r)[:, np.newaxis]
-    time = np.arange(count)[:, np.newaxis] * delta_t_os
+    # Determine the number of points in the trajectory
+    n_points = len(radial_positions)
 
-    r = r[round(oversampling / 2) : count : oversampling]
-    theta = theta[round(oversampling / 2) : count : oversampling]
-    time = time[round(oversampling / 2) : count : oversampling]
+    # Convert lists to numpy arrays
+    angular_positions = np.array(angular_positions)[:, np.newaxis]
+    radial_positions = np.array(radial_positions)[:, np.newaxis]
+    time_points = np.arange(n_points)[:, np.newaxis] * sampling_period_os
 
-    # Keep the length a multiple of 4, to save pain..!
-    count_4 = 4 * int(np.floor(np.shape(theta)[0] / 4))
-    r, theta, time = r[:count_4], theta[:count_4], time[:count_4]
+    # Downsample trajectory to original sampling period
+    downsample_indices = slice(round(oversampling_factor / 2), n_points, oversampling_factor)
+    radial_positions = radial_positions[downsample_indices]
+    angular_positions = angular_positions[downsample_indices]
+    time_points = time_points[downsample_indices]
 
-    # Compute k-space trajectory on regular raster
-    k = r * np.exp(1j * theta)
+    # Adjust length of arrays to be a multiple of 4
+    valid_length = 4 * (len(angular_positions) // 4)
+    radial_positions = radial_positions[:valid_length]
+    angular_positions = angular_positions[:valid_length]
+    time_points = time_points[:valid_length]
 
-    # Calculate gradients by shifting k forward and backward.
-    k_shifted_forward = np.vstack([np.zeros((1, 1), dtype=complex), k])
-    k_shifted_backward = np.vstack([k, np.zeros((1, 1), dtype=complex)])
-    g = (k_shifted_forward - k_shifted_backward)[:-1] / T
+    # Compute k-space trajectory on the regular time raster
+    k_space_trajectory = radial_positions * np.exp(1j * angular_positions)
 
-    # re-calculate trajectory values at the correct time points (1/2 * T, 3/2 * T, ...)
-    # the first point is actually the ramp from 0 to g[0] over T/2. Thus the total factor 1/4.
-    k = -np.cumsum(np.concatenate(([g[0] * T / 4], (g[:-1] + g[1:]) * T / 2)))
+    # Calculate gradient waveform by shifting k-space trajectory
+    k_shifted_forward = np.vstack([np.zeros((1, 1), dtype=complex), k_space_trajectory])
+    k_shifted_backward = np.vstack([k_space_trajectory, np.zeros((1, 1), dtype=complex)])
+    grad_waveform = (k_shifted_forward - k_shifted_backward)[:-1] / sampling_period
 
-    # Compute the slew rate s
-    s = -np.diff(np.vstack([np.zeros((1, 1), dtype=complex), g]), axis=0) / T
+    # Recalculate k-space positions at midpoints between time steps for accuracy
+    initial_point = [grad_waveform[0] * sampling_period / 4]
+    mid_points = (grad_waveform[:-1] + grad_waveform[1:]) * sampling_period / 2
+    k_space_trajectory = -np.cumsum(np.concatenate((initial_point, mid_points)))
 
-    return k.flatten(), g.flatten(), s.flatten(), time.flatten(), r.flatten(), theta.flatten()
+    # Compute final slew rate from the gradient waveform
+    gradient_shifted_backward = np.vstack([np.zeros((1, 1), dtype=complex), grad_waveform])
+    slew_rate = -np.diff(gradient_shifted_backward, axis=0) / sampling_period
+
+    return (
+        k_space_trajectory.flatten(),
+        grad_waveform.flatten(),
+        slew_rate.flatten(),
+        time_points.flatten(),
+        radial_positions.flatten(),
+        angular_positions.flatten(),
+    )
